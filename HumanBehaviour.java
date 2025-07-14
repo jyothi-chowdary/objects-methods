013ad4e8d4b1567e28e0ce2335d677e14f9b8d7d
jyothi-chowdary <jyothigutlapalli33@gmail.com>
2025-07-14 16:57:20 +0530

    Add files via upload

---
+++ HumanBehaviour.java
@@ -0,0 +1,37 @@
+class HumanBehaviour{
+	String name="JYOTHI";
+	static int age=20;
+	public static void listen(){
+	System.out.println("listening");
+	}
+	 
+	public void walk(){
+	 System.out.println("walking");
+
+	}
+          
+	public static void sleep(){
+	System.out.println("sleeping");
+	}
+           
+	public static void knowledge(){
+	System.out.println("knowldge gaining");
+	}
+          
+    	public static void teach(){
+	System.out.println("teaching");
+}
+	  
+public static void main(String[]args){
+HumanBehaviour d1=new HumanBehaviour();
+HumanBehaviour d2=new HumanBehaviour();
+ d1.walk();
+ sleep();
+d1.teach();
+
+
+}
+}
+
+
+
