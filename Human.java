aded319cc4c5499d6428e15f07bb1ef5a6bdd962
jyothi-chowdary <jyothigutlapalli33@gmail.com>
2025-07-14 16:24:18 +0530

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,26 @@
+class Human{
+	String name="JYOTHI";
+	static int age=20;
+	public static void listen(){}
+	 
+	public static void walk(){}
+          
+	public static void sleep(){}
+           
+	public static void knowledge(){}
+          
+    	public static void teach(){}
+	  
+public static void main(String[]args){
+Dog d1=new Dog();
+Dog d2=new Dog();
+System.out.println(d1==d2);
+System.out.println(d2);
+System.out.println(age);
+System.out.println(d1.name);
+System.out.println(d2.name);
+}
+}
+
+
+
